7b95f1e3e46773000e729c558748536e0b9df515
ElideZavala <zavalavinagreelide@gmail.com>
2022-07-31 10:16:40 -0500

    metodos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,5 @@ selector: 'app-root',
 templateUrl: 'app.component.html'
 })
 export class AppComponent {
-  public titulo:string = 'Contador App';
-  public numero:number = 0;
-  public base: number = 5;
 
-  acumular( valor:number ) {
-    this.numero += valor
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ContadorComponet } from "./contador/contador.componet";
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContadorComponet,
+    HeroeComponent
   ],
   imports: [
     BrowserModule
