429f05421bce95ffb809bad45c3343e8ac6d98d0
ElideZavala <zavalavinagreelide@gmail.com>
2022-08-13 23:41:15 -0500

    agregamos un elements de servicio

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 
 @Component({
@@ -10,6 +11,8 @@ import { Personaje } from '../interfaces/dbz.interface';
 export class PersonajesComponent {
 
 @Input() personajes: Personaje[] = []
+
+constructor( private dbzService: DbzService ) { }
 
   
 
